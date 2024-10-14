f21817bd0c28c34c208f55dc99556205fefe6cbb
Andre Borba <dev_andre.filipe@outlook.com>
2024-10-13 23:28:38 -0300

    Update project.clj with dependencies and description

--- project.clj
+++ project.clj
@@ -1,9 +1,15 @@
 (defproject sliceit "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Chunking api"
+:url "https://github.com/af2b/sliceit"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [ring/ring-core "1.12.2"]
+                 [ring/ring-jetty-adapter "1.12.2"]
+                 [ring/ring-defaults "0.3.3"]
+                 [compojure "1.7.1"]
+                 [cheshire "5.13.0"]
+                 [ring/ring-json "0.5.1"]]
   :main ^:skip-aot sliceit.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
