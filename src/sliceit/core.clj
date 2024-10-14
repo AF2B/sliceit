5dd386604dafb922f69c3a18f1d537314a09e259
Andre Borba <dev_andre.filipe@outlook.com>
2024-10-13 23:29:04 -0300

    Refactor core.clj to add app routes and middleware

--- src/sliceit/core.clj
+++ src/sliceit/core.clj
@@ -1,7 +1,15 @@
 (ns sliceit.core
-  (:gen-class))
+  (:gen-class)
+ (:require [ring.adapter.jetty :refer [run-jetty]]
+           [sliceit.routes.app-routes :refer [app-routes]]
+           [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+           [ring.middleware.json :refer [wrap-json-body wrap-json-response]]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def app
+  (-> app-routes
+      (wrap-json-body {:keywords? true})
+      wrap-json-response
+      (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))))
+
+(defn -main []
+  (run-jetty app {:port 3000}))
